95d14b2e0557db65318e99a5b9814a17be41fe50
ferrantejake <jake.ferrante@hotmail.com>
2017-02-10 23:11:03 -0500

    Add dynamo depedencies Add dynamo typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -6,5 +6,6 @@
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="globals/serve-favicon/index.d.ts" />
 /// <reference path="globals/serve-static/index.d.ts" />
+/// <reference path="modules/aws-sdk/index.d.ts" />
 /// <reference path="modules/express-serve-static-core/index.d.ts" />
 /// <reference path="modules/express/index.d.ts" />
