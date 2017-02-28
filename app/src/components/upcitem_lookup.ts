f9f528e3a8d4ba66a3e0b5388d9eb929d1c36019
Fernando Soler-Casanova <fsoler58@knights.ucf.edu>
2017-02-28 15:39:36 -0500

    Ironed out some syntax errors in upcitem_lookup.ts -- still needs semantic work in typings folder

--- app/src/components/upcitem_lookup.ts
+++ app/src/components/upcitem_lookup.ts
@@ -1,50 +1,43 @@
-// Implemenation of nodeJS request module for UPC barcode look up 
-// Through UPCitemDB 
+// Implemenation of nodeJS request module for UPC barcode look up
+// Through UPCitemDB
 
-const require = require('request')
-const https = require('https')
-
-var options = {
-  hostname: 'api.upcitemdb.com',
-  path: '/prod/trial/lookup?upc',
-}
+const require = require('request');
+const https = require('https');
 
 // To contain the UPC to be looked up
 // Default set to Bud Light 6 pack
-var UPC = '018200533082'; 
-// URl for get requests 
-var requestUrl = 'api.upcitemdb.com/prod/trial/lookup?upc='
-var getRequest = requestUrl + UPC
-
-
+let UPC = '018200533082';
+// URl for get requests
+const requestUrl = 'api.upcitemdb.com/prod/trial/lookup?upc=';
+const getRequest = requestUrl + UPC;
 
 request(getRequest, function(error, response, body)
 {
-    if(!error, && response.statusCode == 200)
-    {
-        console.log(body)
+    if (!error && response.statusCode === 200) {
+        console.log(body);
     }
-    else if(error)
-    {
-        if(response.statusCode == 400)
-        {
-            console.log("400: Invalid Query: Missing parameters")
-        }
-        if(response.statusCode == 404)
-        {
-            console.log("404: No matches found")
-        }
-        if(response.statusCode == 429)
-        {
-            console.log("429: Exceeded Request Limit for Day")
-        }
-        if(response.statusCode == 500 )
-        {
-            console.log("500: Server Error")
-        }
+    else
+        switch (response.statusCode) {
+            case 400: {
+                console.log('400: Invalid Query: Missing parameters');
+                break;
+            }
+            case 404: {
+                console.log('404: No matches found');
+                break;
+            }
+            case 429: {
+                console.log('429: Exceeded Request Limit for Day');
+                break;
+            }
+            case 500: {
+                console.log('500: Server Error');
+                break;
+            }
+            default: {
+                console.log('Unknown Error');
+                break;
+            }
     }
 
-
-} )
-
-// Sample Code 
+} );
