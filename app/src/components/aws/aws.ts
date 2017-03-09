bf1a57accfd1731959050a12854401c9f7ff351b
ferrantejake <jake.ferrante@hotmail.com>
2017-03-09 13:12:12 -0500

    Update AWS client to Dynasty

--- app/src/components/aws/aws.ts
+++ app/src/components/aws/aws.ts
@@ -3,17 +3,15 @@
 import * as AWS from 'aws-sdk';
 const debug = require('debug')('holdmybeer:aws');
 
-// Setup AWS client configuration
-const options = {
-    region: process.env.AWS_REGION,
+const config = {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_KEY,
-} as AWS.ClientConfigPartial;
-AWS.config.update(options);
+    region: process.env.AWS_REGION
+};
 
 // Create single instance of dynamodb document client
-const dynamodb = new AWS.DynamoDB.DocumentClient();
+const dynasty = require('dynasty')(config);
 
-export function client(): AWS.DynamoDB.DocumentClient {
-    return dynamodb;
+export function dynamodb(): any {
+    return dynasty;
 };
