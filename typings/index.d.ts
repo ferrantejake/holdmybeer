baa3660cca5f9bed84cfc2624f3ac9bdcd10ad1d
ferrantejake <jake.ferrante@hotmail.com>
2017-03-22 20:07:15 -0400

    General cleanup Promisify app startup sequence

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -3,7 +3,6 @@
 /// <reference path="globals/debug/index.d.ts" />
 /// <reference path="globals/mime/index.d.ts" />
 /// <reference path="globals/morgan/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
 /// <reference path="globals/serve-favicon/index.d.ts" />
 /// <reference path="globals/serve-static/index.d.ts" />
 /// <reference path="modules/aws-sdk/index.d.ts" />
