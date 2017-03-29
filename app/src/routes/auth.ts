551d6bc834a6bd65cb736d28b8163abfda913423
ferrantejake <jake.ferrante@hotmail.com>
2017-03-29 14:58:47 -0400

    Add wireframes for additional utils

--- app/src/routes/auth.ts
+++ app/src/routes/auth.ts
@@ -1,10 +1,25 @@
 import * as express from 'express';
 const router = express.Router();
 const debug = require('debug')('holdmybeer:auth');
-import { cryptoLib } from '../utils';
+import { cryptoLib, rest } from '../utils';
 /* GET home page. */
 
-router.get('/session', (req: express.Request, res: express.Response) => {
+router.get('/session');
+router.get('/status');
+router.get('/login');
+router.get('/logout');
+
+const verify = rest.verify();
+const validate = rest.validate();
+
+router.route('/session')
+    .get(verify, validate, respond(session));
+
+
+
+// Request a new device authentication session.
+function session(req: express.Request, res: express.Response) {
+\
     cryptoLib.generateSessionToken()
         .then(sessionToken => {
             res.json({ token: sessionToken });
@@ -14,6 +29,16 @@ router.get('/session', (req: express.Request, res: express.Response) => {
             debug(error);
             res.status(500);
         });
-});
+}
 
+// Check the status of an account
+function accountStatus(req: express.Request, res: express.Response) { }
+
+// Log into an account using an authorization provider.
+function login(req: express.Request, res: express.Response) {
+
+}
+
+// Log out of an account.
+function logout(req: express.Request, res: express.Response) { }
 module.exports = router;
