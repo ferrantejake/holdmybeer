659c1da25c40469391208470efc138e9538cc849
ferrantejake <jake.ferrante@hotmail.com>
2017-04-03 00:22:31 -0400

    Clean up merge; missed some files

--- app/src/utils/index.ts
+++ app/src/utils/index.ts
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 import * as init from './init';
-export { init };
-=======
 import * as rest from './rest';
 import * as token from './token';
 import * as cryptoLib from './cryptoLib';
 
-export { rest, token, cryptoLib }
->>>>>>> auth-endpoints
+export {init, rest, token, cryptoLib }
