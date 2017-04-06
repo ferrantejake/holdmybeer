314a7ba626bb74ce7c2d732b7abe5d53cc91fefb
ferrantejake <jake.ferrante@hotmail.com>
2017-04-05 22:50:23 -0400

    Add accountStatus

--- app/src/routes/account.ts
+++ app/src/routes/account.ts
@@ -1,4 +1,5 @@
 import { cryptoLib, rest, token } from '../utils';
+import { dq } from '../components';
 import * as express from 'express';
 const router = express.Router();
 const debug = require('debug')('holdmybeer:auth');
@@ -13,6 +14,7 @@ const paramOptions = {
 const verify = rest.verify(paramOptions);
 const validate = rest.validate(paramOptions);
 const respond = rest.respond(debug);
+const getContext = rest.getContext;
 
 router.route('/status')
     .get(accountStatus);
@@ -33,7 +35,13 @@ function session(req: express.Request, res: express.Response): Promise<rest.Resp
 
 // Check the status of an account
 function accountStatus(req: express.Request, res: express.Response): Promise<rest.Response> {
-    return new Promise<rest.Response>((resolve, reject) => { });
+    return new Promise<rest.Response>((resolve, reject) => {
+        getContext(req).then(requestContext => {
+            if (requestContext.user)
+                rest.Response.fromSuccess(dq.UserDq.mapToConsumable(requestContext.user));
+            else rest.Response.fromNotFound({ path: undefined, message: 'Invalid token' });
+        });
+    });
 }
 
 // Log into an account using an authorization provider.
