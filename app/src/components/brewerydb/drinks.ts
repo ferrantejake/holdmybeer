26ffa4c84c88d9cab75a74b1004ab2d7f6f401f0
ferrantejake <jake.ferrante@hotmail.com>
2017-04-08 00:43:14 -0700

    Add getByUPC and package

--- app/src/components/brewerydb/drinks.ts
+++ app/src/components/brewerydb/drinks.ts
@@ -1,3 +1,23 @@
-export function getById(id: string): any {
-    return undefined; // something
+const brewerydb = require('brewerydb-node');
+import * as express from 'express';
+
+export function getByUPC(upc: string): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+        brewerydb.beer.getById(upc, {}, (error: Error, response: express.Response, body: any) => {
+            if (response && response.statusCode === 200)
+                try { resolve(JSON.stringify(body)); }
+                catch (error) { reject(error); }
+            else reject(error);
+        });
+    });
 }
+
+// /beer/:beerId
+// brewdb.beer.getById('avMkil', {}, callback);
+
+// // /beers?ids=
+// brewdb.beer.getById(['avMkil', 'XcvLTe'], { withBreweries: 'Y' }, callback);
+
+// // /beers?name=“bock”&abv=....
+// // can provide params that beers endpoint accepts (like abv, ibu, etc.)
+// brewdb.beer.find({ name: 'bock' }, callback);
