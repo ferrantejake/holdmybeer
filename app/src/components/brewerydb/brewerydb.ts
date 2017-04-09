be9df5b6b3883f7becd9f97468b1f9dacc3c829c
ferrantejake <jake.ferrante@hotmail.com>
2017-04-09 17:40:19 -0400

    Add clietn

--- app/src/components/brewerydb/brewerydb.ts
+++ app/src/components/brewerydb/brewerydb.ts
@@ -1,3 +1,7 @@
-import * as drinks from './drinks';
+// Create brewerydb client interface
+const BreweryDb = require('brewerydb-node');
+const breweryDbClient = new BreweryDb(process.env.BREWERYDB_API_KEY);
 
-export { drinks };
+export function client() {
+    return breweryDbClient;
+}
