7064b72486aca2a8ff743a253c6cab7814e6bbfe
ferrantejake <jake.ferrante@hotmail.com>
2017-04-12 02:40:04 -0400

    Make command inline

--- deploy.cmd
+++ deploy.cmd
@@ -76,7 +76,6 @@ IF DEFINED KUDU_SELECT_NODE_VERSION_CMD (
   SET NPM_CMD="!NODE_EXE!" "!NPM_JS_PATH!"
 ) ELSE (
   SET NPM_CMD=npm
-  SET GULP_CMD=".\node_modules\.bin\gulp.cmd"
   SET NODE_EXE=node
 )
 
@@ -111,7 +110,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
 IF EXIST "%DEPLOYMENT_TARGET%\Gulpfile.js" (
   pushd "%DEPLOYMENT_TARGET%"
   echo "Building web site using Gulp"
-  call :ExecuteCmd !GULP_CMD!
+  call :ExecuteCmd ".\node_modules\.bin\gulp.cmd"
   if !ERRORLEVEL! NEQ 0 goto error
   popd
 )
