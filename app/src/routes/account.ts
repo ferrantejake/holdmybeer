381203245e633e96039c26249a8e1ee8594a0fcd
ferrantejake <jake.ferrante@hotmail.com>
2017-04-12 02:16:32 -0400

    Remove /users Update notAllowed

--- app/src/routes/account.ts
+++ app/src/routes/account.ts
@@ -14,7 +14,7 @@ const paramOptions = {
 const verify = rest.verify(paramOptions);
 const validate = rest.validate(paramOptions);
 const respond = rest.respond(debug);
-const notAllowed = rest.notAllowed(undefined);
+const notAllowed = rest.notAllowed({});
 const getContext = rest.getContext;
 // const getContext: any = undefined; // rest.getContext;
 
--- app/src/routes/index.ts
+++ app/src/routes/index.ts
@@ -5,20 +5,18 @@ import { rest } from '../utils';
 const router = express.Router();
 const debug = require('debug')('holdmybeer:index');
 const pkg = require('../../../package.json');
+const notAllowed = rest.notAllowed({});
 
 // Import endpoints from alternative routes.
 router.use('/account', require('./account'));
 router.use('/beer', require('./beer'));
 
 /* GET home page. */
-
-router.use('/users', require('./users'));
-
 router.get('/', (req: express.Request, res: express.Response) => {
   console.log('serving index...');
   res.render('index', { title: pkg.name, version: pkg.version, description: pkg.description, message: '' });
 });
 
-router.all('*', rest.notAllowed);
+router.all('*', notAllowed);
 
 module.exports = router;
