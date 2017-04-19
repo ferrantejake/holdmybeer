dea8e0c9e27f6532c10130ddddf48e6d168a702c
ferrantejake <jake.ferrante@hotmail.com>
2017-04-19 09:14:02 -0400

    Expose AWS dynamo client

--- app/src/components/aws/aws.ts
+++ app/src/components/aws/aws.ts
@@ -1,15 +1,26 @@
-// import * as AWS from 'aws-sdk';
+import * as AWS from 'aws-sdk';
 const debug = require('debug')('holdmybeer:aws');
 
+// Setup AWS client configuration
 const config = {
+    region: process.env.AWS_REGION,
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_KEY,
-    region: process.env.AWS_REGION
+};
+
+// Update raw AWS interface
+AWS.config.update(config);
+
+// Create single instance of dynasty document client
+const _dynasty = require('dynasty')(config);
+
+export function dynamodb(): any {
+    return _dynasty;
 };
 
 // Create single instance of dynamodb document client
-const dynasty = require('dynasty')(config);
+const _dynamodb = new AWS.DynamoDB.DocumentClient();
 
-export function dynamodb(): any {
-    return dynasty;
+export function AWSDynamoClient(): AWS.DynamoDB.DocumentClient {
+    return _dynamodb;
 };
